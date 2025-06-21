ea56b9c8c3a084a1e509eddaa3868faf89cc9021
ericdodds <eric@ericdodds.com>
2025-06-21 11:43:06 +0300

    adjusted the style of the dates

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,14 +1,14 @@
 import { BlogPosts } from 'app/components/posts'
 
 export const metadata = {
-  title: 'Blog',
+  title: 'Eric Dodds Weblog',
   description: 'Read my blog.',
 }
 
 export default function Page() {
   return (
     <section>
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Blog</h1>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Eric Dodds Weblog</h1>
       <BlogPosts />
     </section>
   )
--- app/layout.tsx
+++ app/layout.tsx
@@ -11,15 +11,15 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'Eric Dodds Weblog',
+    template: '%s | Eric Dodds Weblog',
   },
-  description: 'This is my portfolio.',
+  description: 'Welcome to ericdodds.com.',
   openGraph: {
-    title: 'My Portfolio',
-    description: 'This is my portfolio.',
+    title: 'Eric Dodds WEblog',
+    description: 'Welcome to ericdodds.com.',
     url: baseUrl,
-    siteName: 'My Portfolio',
+    siteName: 'Eric Dodds Weblog',
     locale: 'en_US',
     type: 'website',
   },
